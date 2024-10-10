2e26cbabe2da19a328920fb09085c84a5966716a
Lymar Volodymyr <shaxtar11@gmail.com>
2024-10-10 12:50:08 +0200

    Added i18n to support multilingualism

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import asyncio
 
-from handlers import register_routers
-from config.bot_config import bot, dp
+from aiogram.utils.i18n.middleware import SimpleI18nMiddleware
+
+from config.bot_config import bot, dp, i18
 from db.models.models import async_main
+from handlers import register_routers
 
 
 async def main() -> None:
     await async_main()
+    dp.update.middleware(SimpleI18nMiddleware(i18n=i18))
     register_routers(dp)
     await dp.start_polling(bot)
 
